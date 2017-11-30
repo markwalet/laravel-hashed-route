22c26e33aed81e2ba3ff7d92cf20bf99a5cd0357
Mark Walet <mark.walet@gmail.com>
2017-11-30 15:37:51 +0100

    Add comments to config file

--- config/hashed-route.php
+++ config/hashed-route.php
@@ -2,9 +2,36 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default Transformer configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the hashed route transformers below you wish
+    | to use as your default transformer for all your models. You can also
+    | set a transformer per model or fetch one manually using the manager.
+    |
+    */
+
     'default' => 'hashids',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Transformer configurations
+    |--------------------------------------------------------------------------
+    |
+    | This is a list of all transformer configurations for all your models.
+    | There is an example of options for every driver to make development
+    | easy. You can change them however you like.
+    |
+    */
+
     'transformers' => [
+
+        'none' => [
+            'driver' => 'null',
+        ],
+
         'hashids' => [
             'driver' => 'hashids',
             'salt' => env('HASHIDS_SALT'),
@@ -12,8 +39,5 @@ return [
             'alphabet' => null,
             //'alphabet' => 'abcdefghijklmnopqrstuvwxyz1234567890'
         ],
-        'none' => [
-            'driver' => 'null',
-        ]
-    ]
+    ],
 ];
